d755f303513bb984ed420edef7741788b78e63d4
hulkchou <hellonick761005@gmail.com>
2017-03-12 13:26:29 +0800

    First homework

---
+++ MyAccounting/Controllers/AccountingController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyAccounting.Models;
+
+namespace MyAccounting.Controllers
+{
+    public class AccountingController : Controller
+    {
+        // GET: Accounting
+        public ActionResult Index()
+        {
+            var AccountingList = new List<AccountingViewModels>
+            {
+                new AccountingViewModels{Type = "支出",date = Convert.ToDateTime("2016-01-01"), price = 300 },
+                new AccountingViewModels{Type = "支出",date = Convert.ToDateTime("2016-01-02"), price = 16000 },
+                new AccountingViewModels{Type = "支出",date = Convert.ToDateTime("2016-01-03"), price = 8000 }
+            };
+
+            return View(AccountingList);
+        }
+
+        // GET: Accounting/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Accounting/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accounting/Create
+        [HttpPost]
+        public ActionResult Create(CreateAccountingViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Accounting/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Accounting/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Accounting/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Accounting/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MyAccounting/Models/AccountingViewModels.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyAccounting.Models
+{
+    public class AccountingViewModels
+    {
+        public string Type { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal price { get; set; }
+
+        public System.DateTime date { get; set; }
+
+        public string Note { get; set; }
+    }
+
+    public class CreateAccountingViewModel
+    {
+        [Display(Name = "類別")]
+        public string Type { get; set; }
+
+        [Display(Name = "金額")]
+        [DataType(DataType.Currency)]
+        public string price { get; set; }
+
+        [Display(Name = "日期")]
+        public System.DateTime date { get; set; }
+
+        [Display(Name = "備註")]
+        public string Note { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public enum EnumType
+    {
+        支出,
+        收入
+    }
+}
